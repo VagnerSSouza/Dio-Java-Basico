16adb302e6c291bb14798b89a2ae5c92367f154e
Vagner Souza <vagner.hyp@gmail.com>
2024-07-28 20:27:44 -0300

    com

---
+++ Variaveis/src/Variaveis.java
@@ -0,0 +1,23 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+        
+        double salarioMinimo = 2500;
+       
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+      /*  int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero); */
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+    }
+}
